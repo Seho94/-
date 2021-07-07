32a56f44bda1f5b3d66e8b42a460e4e71219de18
Seho94 <72437691+Seho94@users.noreply.github.com>
2021-07-07 21:07:18 +0900

    Add files via upload
    210707 Python프로그래밍2

---
+++ Python프로그래밍2/mymodule/myprint2.py
@@ -0,0 +1,9 @@
+def print3(st):
+    print('*' * 50)
+    print(st)
+    print('-' * 50)
+
+def print4(st):
+    print("-"*50)
+    print(st)
+    print("*" * 50)
---
+++ Python프로그래밍2/myprint.py
@@ -0,0 +1,9 @@
+def print1(st):
+    print('*' * 50)
+    print(st)
+    print('*' * 50)
+
+def print2(st):
+    print("-"*50)
+    print(st)
+    print("-" * 50)
